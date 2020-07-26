ebb7b89248e8d7aac2064a702708c9b9e3152f8f
Ali rehman <ar5557487@gmail.com>
2020-07-26 13:09:42 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import QuestionCard from './components/QuestionCard';
+import {fetchQuestions, Difficulty, QuestionState} from './API';
+import styled from 'styled-components';
 import './App.css';
 
+const TOTAL_QUESTIONS = 10;
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correct_answer: string;
+};
+
 function App() {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswer, setUserAnswer] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log(questions);
+
+  const startQuiz = async () => {
+    setLoading(true);
+    setGameOver(false);
+    const newQuestions = await fetchQuestions(TOTAL_QUESTIONS, Difficulty.EASY);
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswer([]);
+    setNumber(0);
+
+    setLoading(false);
+  };
+
+  const nextQuestion = async () => {
+    setNumber(number + 1);
+  };
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    let obj = {
+      question: questions[number].question,
+      answer: e.currentTarget.innerText,
+      correct: false,
+      correct_answer: questions[number].correct_answer,
+    };
+
+    if (e.currentTarget.innerText === questions[number].correct_answer) {
+      obj.correct = true;
+      setScore(score + 1);
+    }
+
+    setUserAnswer(() => {
+      return userAnswer.concat([obj]);
+    });
+  };
+
+  const Title = styled.h1`
+    font-size: 3em;
+    text-align: center;
+    color: rgb(0, 64, 107);
+  `;
+
+  const Button = styled.button`
+    border-radius: 3px;
+    border: 2px solid rgb(0, 64, 107);
+    fontweight: bold;
+    margin: 1em 2em;
+    padding: 0.15em 1em;
+    background: rgb(19, 128, 201);
+    font-size: 1em;
+    color: white;
+    text-align: center;
+  `;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={'app'}>
+      <Title>Quiz Time!</Title>
+      {gameOver || userAnswer.length === TOTAL_QUESTIONS ? (
+        <Button className="start" onClick={startQuiz}>
+          Begin Quiz
+        </Button>
+      ) : null}
+      {!gameOver ? <Title className="score">Score:{score}</Title> : null}
+      {loading ? <p>Loading</p> : null}
+      {!loading && !gameOver ? (
+        <QuestionCard
+          questionNum={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswer ? userAnswer[number] : undefined}
+          callback={checkAnswer}
+        />
+      ) : null}
+
+      {!gameOver &&
+      !loading &&
+      userAnswer.length === number + 1 &&
+      number !== TOTAL_QUESTIONS - 1 ? (
+        <Button className="next" onClick={nextQuestion}>
+          Next
+        </Button>
+      ) : null}
     </div>
   );
 }
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import styled from 'styled-components';
+import './Question.css';
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNum: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNum,
+  totalQuestions,
+}) => {
+  const Title = styled.h1`
+    font-size: 1.8em;
+    text-align: center;
+    color: rgb(77, 13, 57);
+  `;
+
+  const Button = styled.button`
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border-radius: 3px;
+
+    /* Color the border and text with theme.main */
+    color: ${(props) => props.theme.main};
+    border: 2px solid ${(props) => props.theme.main};
+  `;
+
+  // Define what main theme will look like
+  const theme = {
+    main: 'mediumseagreen',
+  };
+
+  return (
+    <div className={'quest'}>
+      <Title>
+        Question:{questionNum}/{totalQuestions}
+      </Title>
+      <Title dangerouslySetInnerHTML={{__html: question}} />
+      <div>
+        {answers.map((answer) => {
+          return (
+            <div>
+              <Button
+                theme={{main: 'royalblue'}}
+                disabled={userAnswer}
+                onClick={callback}
+              >
+                <span dangerouslySetInnerHTML={{__html: answer}} />
+              </Button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
